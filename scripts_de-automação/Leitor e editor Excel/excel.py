a014b3a1ae9c9add0c4b473d892ecb47b60a4cec
Lvdstr <133938106+Lvdstr@users.noreply.github.com>
2024-12-18 12:26:39 -0300

    Add files via upload

--- scripts_de-automação/Leitor e editor Excel/excel.py
+++ scripts_de-automação/Leitor e editor Excel/excel.py
@@ -1,7 +1,12 @@
 from os import system, name
 from colorama import Fore
 from openpyxl import load_workbook
+#from functions import verifiy_system
 
+try:
+        workbook = load_workbook('registros.xlsx')
+except:
+        print("o arquivo foi removido patrão")
 
 mensagem = f"""
 qual planilha quer abrir:
@@ -11,24 +16,23 @@ qual planilha quer abrir:
 4- {Fore.GREEN + "jogos"+ Fore.RESET}
 """
 
-
-try:
-        workbook = load_workbook('/data/data/com.termux/files/home/scripts/exceu/registros.xlsx')
-except:
-        print("o arquivo foi removido patrão")
-
-
 def alterar_valor(planilha):
-	sheet = workbook[planilha]
-	celula = input("digite nome da célula que quer modificar: ")
-	if celula == "":
-		system('clear')
-	else:
-		old_value = sheet[celula].value
-		new_value = input(f"digite o novo valor da celula {celula}: ").upper()
-		sasa = sheet[celula] = new_value
-		workbook.save('/data/data/com.termux/files/home/scripts/exceu/registros.xlsx')
-		print(f"valor antigo: {old_value}\nnovo valor: {new_value}")
+        sheet = workbook[planilha]
+        celula = input("digite nome da célula que quer modificar: ")
+        if celula == "":
+                #verifiy_system()
+                print("se foda")
+        else:
+                old_value = sheet[celula].value
+                new_value = input(f"digite o novo valor da celula {celula}: ").upper()
+                match new_value.isdigit():
+                        case True:
+                                new_value = int(new_value)
+                        case _:
+                                pass
+                sasa = sheet[celula] = new_value
+                workbook.save('registros.xlsx')
+                print(f"valor antigo: {old_value}\nnovo valor: {new_value}")
 
 
 def exibir_planilha(planilha, row, column):
@@ -36,6 +40,10 @@ def exibir_planilha(planilha, row, column):
         if planilha == "FILMES" or planilha == "JOGOS":
                 sheet = workbook[planilha]
                 while row < 100:
+                        #gambiarra feita aqui, arrume dps, feita pra rodar todas as planilhas
+                        #senão ou pega as planilhas 1,2 e 4 e não a 3 ou vice versa
+                        #tem haver com a 3 ter uma lista de parametros e as outras não
+                        #corrigi ai nathan do futuro
                         cell = Fore.GREEN + column[0] + str(row) + Fore.RESET
                         cell_value = sheet[column[0] + str(row)].value
                         if cell_value == None:
@@ -47,9 +55,13 @@ def exibir_planilha(planilha, row, column):
 
         elif planilha == "MANGAS":
                 sheet = workbook[planilha]
-                cell = Fore.GREEN + "  A:nomes       C:capitulos lidos" + Fore.RESET
+                cell = Fore.GREEN + "   A:nomes       C:capitulos lidos" + Fore.RESET
                 print(cell)
                 while row < 100:
+                        #gambiarra feita aqui, arrume dps, feita pra rodar todas as planilhas
+                        #senão ou pega as planilhas 1,2 e 4 e não a 3 ou vice versa
+                        #tem haver com a 3 ter uma lista de parametros e as outras não
+                        #corrigi ai nathan do futuro
                         cell_value = sheet[column[0] + str(row)].value
                         cell_sasa = sheet[column[1] + str(row)].value
                         if cell_value == None:
@@ -75,18 +87,24 @@ def exibir_planilha(planilha, row, column):
 
 def chamar_planilha():
                 choice = input(mensagem)
+
                 match choice:
                         case "1":
                                 exibir_planilha('FILMES', 2, 'a')
+
                         case "2":
                                 exibir_planilha('MANGAS', 2, ['a', "c"])
+
                         case "3":
                                 exibir_planilha("DIGITAL TAMERS", 10, ["c", "d"])
+
                         case "4":
                                 exibir_planilha('JOGOS', 4, 'c')
+
                         case _:
                                 pass
                                 #verifiy_system()
 
 
 chamar_planilha()
+#system("python3 /data/data/com.termux/files/home/scripts/python/corrigir_caps.py")
