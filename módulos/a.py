53f31554f73dd1b132b5ce10540273db3403c918
Lvdstr <133938106+Lvdstr@users.noreply.github.com>
2025-01-01 23:52:00 -0300

    Add files via upload

---
+++ módulos/a.py
@@ -0,0 +1,9 @@
+from jeison import *
+
+
+# teste básico das funções da biblioteca
+# dicio = converter_json("sukuna.json")
+# for x in dicio:
+#     print(f"{x}: {list_convert(dicio[x])}")
+
+# exibir_json_lista("sukuna.json", 1)
